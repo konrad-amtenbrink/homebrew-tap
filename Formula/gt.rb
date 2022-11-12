5312331f78ea0abf6a6df73a26dccc117cfa8727
Konrad Amtenbrink <konrad02@gmx.de>
2022-11-12 23:03:58 +0100

    feat: update gt

--- Formula/gt.rb
+++ Formula/gt.rb
@@ -1,9 +1,9 @@
 class Gt < Formula
   desc "CLI for copying git commit messages"
   homepage "https://github.com/konrad-amtenbrink/gt"
-  url "https://github.com/konrad-amtenbrink/gt/releases/download/v0.1.0/gt-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "22e8249acd509bd4be3a4753311500b5360185f4c9320e34f64542bc0d380729"
-  version "0.1.0"
+  url "https://github.com/konrad-amtenbrink/gt/releases/download/v0.1.1/gt-0.1.1-x86_64-apple-darwin.tar.gz"
+  sha256 "c72d53e3f597fa6ada19b9cfe49fd9d134949a06d1c2e27163adc21f7d996721"
+  version "0.1.1"
 
   def install
     bin.install "gt"
